cfd4eaa26e036707a4126788619fb7f7e11b4a73
Kevin <k.lassauniere@live.fr>
2022-03-15 15:55:00 +0100

    Add playerSelection() - ask user input and return it

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function computerPlay() {
     return randomChoice;
 }
 
+function playerSelection(){
+    const selection = prompt("Make your choice ! (write 'rock', 'paper' or 'scissors'): ");
+    // Check if user input is correct
+    if (selection.toLowerCase() === 'rock' || selection.toLowerCase() === 'paper' || selection.toLowerCase() === 'scissors') {
+        return selection.toLowerCase();
+    } else {
+        console.log("Wrong input.");
+        playerSelection();
+    }
+}
+
+const playerChoice = playerSelection();
+console.log(playerChoice);
