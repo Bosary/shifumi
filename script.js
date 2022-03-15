b4ddfb2405bcb4e2a702aea5c8984e75b18995dc
Kevin <k.lassauniere@live.fr>
2022-03-15 15:43:23 +0100

    Add computerPlay() - randomize computer choice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+function computerPlay() {
+    // Create the choice
+    let computerHand = ['Rock', 'Paper', 'Scissors'];
+
+    // Randomly pick
+    const randomChoice = computerHand[Math.floor(Math.random() * computerHand.length)];
+
+    return randomChoice;
+}
+
