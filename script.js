4498e42b2a93fff3cd578a33dc961082144d2ea3
Kevin <k.lassauniere@live.fr>
2022-03-17 10:53:31 +0100

    Big change. Rewrite computer/player choice to ease victory check. Write compareHands

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// 3 possibles choices. The 2 arrays are different to help checking victory conditions
+const computerHand = ['rock', 'paper', 'scissors'];
+const playerHand = ['paper', 'scissors', 'rock'];
+
+
 function computerPlay() {
-    // Create the choice
-    let computerHand = ['Rock', 'Paper', 'Scissors'];
 
     // Randomly pick
     const randomChoice = computerHand[Math.floor(Math.random() * computerHand.length)];
@@ -8,11 +11,13 @@ function computerPlay() {
     return randomChoice;
 }
 
+const computerChoice = computerPlay();
+
 function playerSelection(){
-    const selection = prompt("Make your choice ! (write 'rock', 'paper' or 'scissors'): ");
+    const selection = prompt("Make your choice ! (write 'rock', 'paper' or 'scissors'): ").toLowerCase();
     // Check if user input is correct
-    if (selection.toLowerCase() === 'rock' || selection.toLowerCase() === 'paper' || selection.toLowerCase() === 'scissors') {
-        return selection.toLowerCase();
+    if (playerHand.includes(selection)) {
+        return selection;
     } else {
         console.log("Wrong input.");
         playerSelection();
@@ -20,4 +25,16 @@ function playerSelection(){
 }
 
 const playerChoice = playerSelection();
-console.log(playerChoice);
+
+function compareHands(playerChoice, computerChoice){
+    //  The string is check for draw. The index are used to check victory
+    if (playerChoice == computerChoice) {
+        console.log(`${playerChoice} VS ${computerChoice}. It's a draw`);
+    } else if (playerHand.indexOf(playerChoice) == computerHand.indexOf(computerChoice)) {
+        console.log(`${playerChoice} VS ${computerChoice}. You win !!!!`);
+    } else {
+        console.log(`${playerChoice} VS ${computerChoice}. You lose !`);
+    }
+}
+
+compareHands(playerChoice, computerChoice);
